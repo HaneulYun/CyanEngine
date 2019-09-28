f321fc5abadebb6dd225c57ec8bc8405b7b837fb
HaneulYun <yhnil.kr@gmail.com>
2019-09-28 19:05:49 +0900

    sub 0.0.1 화면 크기 조정 (예제형태)

--- CyanEngine/CyanEngine.cpp
+++ CyanEngine/CyanEngine.cpp
@@ -73,8 +73,12 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   DWORD dwStyle = WS_OVERLAPPED | WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU | WS_BORDER;
+   RECT rc = { 0, 0, 640, 480 };
+   AdjustWindowRect(&rc, dwStyle, FALSE);
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, dwStyle,
+      CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
