1312ca112a4af779c911e43326cd6c5b02340929
HaneulYun <yhnil.kr@gmail.com>
2019-10-05 01:35:28 +0900

    sub 0.0.23-B 쪼꼬미들 그려주기

--- CyanEngine/Scene.cpp
+++ CyanEngine/Scene.cpp
@@ -66,28 +66,28 @@ void Scene::BuildObjects(ID3D12Device* pd3dDevice)
 
 	m_pd3dGraphicsRootSignature = CreateGraphicsRootSignature(pd3dDevice);
 
-	//Quad* pQuadMesh = new Quad(pd3dDevice, Renderer::Instance()->m_pd3dCommandList);
-	//GameObject* pGameObject = new GameObject();
-	//pGameObject->SetMesh(pQuadMesh);
-	//
-	//TriangleMesh* pMesh = new TriangleMesh(pd3dDevice, Renderer::Instance()->m_pd3dCommandList);
-	//RotatingObject* pRotatingObject = new RotatingObject();
-	//pRotatingObject->SetMesh(pMesh);
-	//
-	//CubeMeshDiffused* pCubeMesh = new CubeMeshDiffused(pd3dDevice, Renderer::Instance()->m_pd3dCommandList, 1, 1, 1);
-	//RotatingObject* pRotatingObject2 = new RotatingObject();
-	//pRotatingObject2->SetMesh(pCubeMesh);
-	//
-	//PlayerShader* pShader = new PlayerShader();
-	//pShader->CreateShader(pd3dDevice, m_pd3dGraphicsRootSignature);
-	//pShader->CreateShaderVariables(pd3dDevice, Renderer::Instance()->m_pd3dCommandList);
-	//pGameObject->SetShader(pShader);
-	//pRotatingObject->SetShader(pShader);
-	//pRotatingObject2->SetShader(pShader);
-	//
-	//gameObjects.push_back(pGameObject);
-	//gameObjects.push_back(pRotatingObject);
-	//gameObjects.push_back(pRotatingObject2);
+	Quad* pQuadMesh = new Quad(pd3dDevice, Renderer::Instance()->m_pd3dCommandList);
+	GameObject* pGameObject = new GameObject();
+	pGameObject->SetMesh(pQuadMesh);
+	
+	TriangleMesh* pMesh = new TriangleMesh(pd3dDevice, Renderer::Instance()->m_pd3dCommandList);
+	RotatingObject* pRotatingObject = new RotatingObject();
+	pRotatingObject->SetMesh(pMesh);
+	
+	CubeMeshDiffused* pCubeMesh = new CubeMeshDiffused(pd3dDevice, Renderer::Instance()->m_pd3dCommandList, 1, 1, 1);
+	RotatingObject* pRotatingObject2 = new RotatingObject();
+	pRotatingObject2->SetMesh(pCubeMesh);
+	
+	PlayerShader* pShader = new PlayerShader();
+	pShader->CreateShader(pd3dDevice, m_pd3dGraphicsRootSignature);
+	pShader->CreateShaderVariables(pd3dDevice, Renderer::Instance()->m_pd3dCommandList);
+	pGameObject->SetShader(pShader);
+	pRotatingObject->SetShader(pShader);
+	pRotatingObject2->SetShader(pShader);
+	
+	gameObjects.push_back(pGameObject);
+	gameObjects.push_back(pRotatingObject);
+	gameObjects.push_back(pRotatingObject2);
 
 
 	m_pd3dGraphicsRootSignature = CreateGraphicsRootSignature(pd3dDevice);
