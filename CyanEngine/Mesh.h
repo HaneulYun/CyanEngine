0679898d244979e470fdcabd57d6d3de8f823927
HaneulYun <yhnil.kr@gmail.com>
2019-11-07 23:12:23 +0900

    sub 0.1.10 부분적 라이팅 적용

--- CyanEngine/Mesh.h
+++ CyanEngine/Mesh.h
@@ -141,4 +141,4 @@ class CubeMeshIlluminated : public MeshIlluminated
 public:
 	CubeMeshIlluminated(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, float fWidth = 2.0f, float fHeight = 2.0f, float fDepth = 2.0f);
 	virtual ~CubeMeshIlluminated() { }
-};
+}; 
