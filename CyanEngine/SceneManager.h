460d944b95611b2ee92683787acdb91730ba1770
HaneulYun <yhnil.kr@gmail.com>
2019-11-12 12:30:50 +0900

    sub 0.2.6 ScriptForm 템플릿 클래스 제공

---
+++ CyanEngine/SceneManager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "framework.h"
+
+class SceneManager : public Component
+{
+private:
+
+public:
+
+private:
+	friend class GameObject;
+	SceneManager() {}
+
+public:
+	~SceneManager() {}
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+};
+
