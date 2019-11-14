6311be56ec0872ea70eeb2ab5ecc79f92100100e
HaneulYun <yhnil.kr@gmail.com>
2019-11-14 14:57:01 +0900

    sub 0.2.19 관찰 : 런타임 Instantiate

--- CyanEngine/SceneManager.h
+++ CyanEngine/SceneManager.h
@@ -18,23 +18,37 @@ public:
 	void Start()
 	{
 		int xObjects = 1, yObjects = 1, zObjects = 1, i = 0;
-		for (int x = -xObjects; x <= xObjects; x++)
-			for (int y = -yObjects; y <= yObjects; y++)
-				for (int z = -zObjects; z <= zObjects; z++)
-				{
-					GameObject* instance = Instantiate(gameObject);
-
-					instance->transform->position = XMFLOAT3{ 10.0f * x, 10.0f * y, 10.0f * z };
-					//instance->GetComponent<Transform>()->position = XMFLOAT3{ 10.0f * x, 10.0f * y, 10.0f * z };
-					
-					instance->GetComponent<RotatingBehavior>()->speedRotating = 0;// 10.0f * (i++ % 10);
-				}
+		//for (int x = -xObjects; x <= xObjects; x++)
+		//	for (int y = -yObjects; y <= yObjects; y++)
+		//		for (int z = -zObjects; z <= zObjects; z++)
+		//		{
+		//			GameObject* instance = Instantiate(gameObject);
+		//
+		//			instance->transform->position = Vector3{ 10.0f * x, 10.0f * y, 10.0f * z };
+		//			//instance->GetComponent<Transform>()->position = XMFLOAT3{ 10.0f * x, 10.0f * y, 10.0f * z };
+		//			
+		//			instance->GetComponent<RotatingBehavior>()->speedRotating = 0;// 10.0f * (i++ % 10);
+		//		}
 	}
 
 	void Update()
 	{
 		static float time = 0;
+		static const float boundary = 0.05;
 		time += Time::deltaTime;
+
+		if (time > boundary)
+		{
+			auto instance = Instantiate(gameObject);
+			float x = Random::Range(-500, 500);
+			float y = Random::Range(-500, 500);
+			float z = Random::Range(10, 500);
+			instance->transform->position = Vector3{ x, y, z };
+
+			instance->GetComponent<RotatingBehavior>()->speedRotating = 90;
+
+			time -= boundary;
+		}
 	}
 
 	virtual Component* Duplicate() { return new SceneManager; };
