efc9ff72c4f2b044f86f3d921eb1b26115b31df4
Kitty <xhwlddj98@gmail.com>
2019-11-16 01:33:30 +0900

    0.0.6 예은0.0) Message 구조체 추가

---
+++ MagentaServer/Message.cpp
@@ -0,0 +1,9 @@
+#include "Message.h"
+
+struct Message
+{
+	char msgId;
+	int lParam;
+	int mParam;
+	int rParam;
+};
--- MagentaServer/ThreadPool.h
+++ MagentaServer/ThreadPool.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <vector>
+#include <queue>
 #include "Singleton.h"
 #include "MainThread.h"
 #include "ConnectingThread.h"
 #include "MessagingThread.h"
+#include "Message.h"
 
 using namespace std;
 
@@ -13,6 +15,9 @@ private:
 	static vector<Thread*> threads;
 	static int nThreads;
 	static int maxThreads;
+
+	static queue<Message> recvQueue;
+	static vector<queue<Message>*> sendQueues;
 
 public:
 	ThreadPool(int num, SOCKET* listen_sock);
