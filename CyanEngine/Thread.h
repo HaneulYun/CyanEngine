b2d54e615069456b8ea26129db194b34e1d77893
jeehye <wangjh79@kpu.ac.kr>
2019-11-17 01:57:56 +0900

    0.0.17 지혜) Message.h 추가, Recvqueue 추가

--- CyanEngine/Thread.h
+++ CyanEngine/Thread.h
@@ -3,14 +3,7 @@
 #pragma once
 #include <winsock2.h>
 #include "framework.h"
-
-struct Message
-{
-	char msgId;
-	int lParam;
-	int mParam;
-	int rParam;
-};
+#include "Message.h"
 
 class Thread : public Component
 {
@@ -64,7 +57,10 @@ private:
 			if (retval == SOCKET_ERROR) {
 				;//err_display("recv()");
 			}
-			printf("recvbuf: %c, %d, %d, %d\n", buf.msgId, buf.lParam, buf.mParam, buf.rParam);
+			//printf("recvbuf: %c, %d, %d, %d\n", buf.msgId, buf.lParam, buf.mParam, buf.rParam);
+			EnterCriticalSection(&rqcs);
+			recvQueue.push(buf);
+			LeaveCriticalSection(&rqcs);
 		}
 		return 0;
 	}
@@ -74,6 +70,8 @@ public:
 		// closesocket()
 		closesocket(sock);
 		WSACleanup();
+
+		DeleteCriticalSection(&rqcs);
 	}
 	virtual Component* Duplicate() { return new Thread; }
 	virtual Component* Duplicate(Component* component) { return new Thread(*(Thread*)component); }
@@ -81,6 +79,8 @@ public:
 	void Start()
 	{
 		// 초기화 코드를 작성하세요.		
+		InitializeCriticalSection(&rqcs);
+
 		retval = WSAStartup(MAKEWORD(2, 2), &wsa);
 		if(retval != 0)
 			err_quit("WSAStartup error");
