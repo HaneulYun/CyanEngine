115e07e98e6c0d885e1ed51070c7236d454b2830
Kitty <xhwlddj98@gmail.com>
2019-11-17 01:11:26 +0900

    0.0.15 예은0.2) 메시지 추가 중...

--- MagentaServer/Message.h
+++ MagentaServer/Message.h
@@ -1,10 +1,5 @@
 #pragma once
 
-// 임시 메시지
-#define ADD 0x0041
-#define SUB 'B'
-#define EQUAL 'E'
-
 struct Message
 {
 	char msgId;
@@ -12,3 +7,20 @@ struct Message
 	int mParam;
 	int rParam;
 };
+
+// 임시 메시지
+// Client to Server
+#define ADD 0x0041
+#define SUB 0x0042
+// Server to Clients
+#define EQUAL 0x0045
+
+//////////////////////////////////////////////
+// Client to Server
+#define MESSAGE_READY 0x0009	// 일단 보류
+
+
+//////////////////////////////////////////////
+// Server to Clients
+#define MESSAGE_YOUR_ID 0x0000	// 클라이언트의 아이디 반환
+#define MESSAGE_CONNECTED_IDS 0x0001 // 서버와 연결된 클라이언트들 ID 반환.
