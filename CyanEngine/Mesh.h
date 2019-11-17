5318242d56642a88e37fc80e75c8850a610d63d6
HaneulYun <yhnil.kr@gmail.com>
2019-11-18 02:18:13 +0900

    sub 0.3.8 모델 로드 관련 메쉬 구현(예제형태)

--- CyanEngine/Mesh.h
+++ CyanEngine/Mesh.h
@@ -17,6 +17,8 @@ protected:
 	UINT m_nStride = 0;
 	UINT m_nOffset = 0;
 
+	UINT m_nType = 0;
+
 protected:
 	ComPtr<ID3D12Resource> m_pd3dIndexBuffer{ nullptr };
 	ComPtr<ID3D12Resource> m_pd3dIndexUploadBuffer{ nullptr };
@@ -121,4 +123,77 @@ public:
 	virtual float OnGetHeight(int x, int z, void *pContext);
 	//격자의 좌표가 (x, z)일 때 교점(정점)의 색상을 반환하는 함수이다.
 	virtual XMFLOAT4 OnGetColor(int x, int z, void *pContext);
+};
+
+#define VERTEXT_POSITION			0x01
+#define VERTEXT_COLOR				0x02
+#define VERTEXT_NORMAL				0x04
+
+class CMeshLoadInfo
+{
+public:
+	CMeshLoadInfo() { }
+	~CMeshLoadInfo();
+
+public:
+	char		m_pstrMeshName[256] = { 0 };
+
+	UINT		m_nType = 0x00;
+
+	XMFLOAT3	m_xmf3AABBCenter = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	XMFLOAT3	m_xmf3AABBExtents = XMFLOAT3(0.0f, 0.0f, 0.0f);
+
+	int			m_nVertices = 0;
+	XMFLOAT3*	m_pxmf3Positions = NULL;
+	XMFLOAT4*	m_pxmf4Colors = NULL;
+	XMFLOAT3*	m_pxmf3Normals = NULL;
+
+	int			m_nIndices = 0;
+	UINT*		m_pnIndices = NULL;
+
+	int			m_nSubMeshes = 0;
+	int*		m_pnSubSetIndices = NULL;
+	UINT**		m_ppnSubSetIndices = NULL;
+};
+
+class CMeshFromFile : public Mesh
+{
+public:
+	CMeshFromFile(CMeshLoadInfo* pMeshInfo);
+	virtual ~CMeshFromFile();
+
+public:
+	virtual void ReleaseUploadBuffers();
+
+protected:
+	ID3D12Resource*				m_pd3dPositionBuffer = NULL;
+	ID3D12Resource*				m_pd3dPositionUploadBuffer = NULL;
+	D3D12_VERTEX_BUFFER_VIEW	m_d3dPositionBufferView;
+
+	int							m_nSubMeshes = 0;
+	int*						m_pnSubSetIndices = NULL;
+
+	ID3D12Resource**			m_ppd3dSubSetIndexBuffers = NULL;
+	ID3D12Resource**			m_ppd3dSubSetIndexUploadBuffers = NULL;
+	D3D12_INDEX_BUFFER_VIEW*	m_pd3dSubSetIndexBufferViews = NULL;
+
+public:
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, int nSubSet);
+};
+
+class CMeshIlluminatedFromFile : public CMeshFromFile
+{
+public:
+	CMeshIlluminatedFromFile(CMeshLoadInfo* pMeshInfo);
+	virtual ~CMeshIlluminatedFromFile();
+
+	virtual void ReleaseUploadBuffers();
+
+protected:
+	ID3D12Resource* m_pd3dNormalBuffer = NULL;
+	ID3D12Resource* m_pd3dNormalUploadBuffer = NULL;
+	D3D12_VERTEX_BUFFER_VIEW		m_d3dNormalBufferView;
+
+public:
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, int nSubSet);
 };
