07fd988d7437cf67ce1fd7acd53ca37b341e110a
HaneulYun <yhnil.kr@gmail.com>
2019-11-19 00:29:12 +0900

    sub 0.3.14 관찰 : 로드된 모델의 인스턴싱 필요

--- CyanEngine/GameScene.cpp
+++ CyanEngine/GameScene.cpp
@@ -8,25 +8,26 @@ void GameScene::BuildObjects()
 	Material* defaultMaterial = new DefaultMaterial();
 	defaultMaterial->shader = new StandardShader();
 
-	//GameObject* terrain = AddGameObject();
-	//{
-	//	XMFLOAT3 scale(2.0f, 0.5f, 2.0f);
-	//	XMFLOAT4 color(0.0f, 0.2f, 0.0f, 0.0f);
-	//
-	//	terrain->GetComponent<Transform>()->position.x = -128*2;
-	//	terrain->GetComponent<Transform>()->position.y = -100.f;
-	//	terrain->AddComponent<Terrain>()->LoadTerrain(L"heightMap.raw", 257, 257, 257, 257, scale, color);
-	//	terrain->AddComponent<Renderer>()->material = defaultMaterial;
-	//	terrain->GetComponent<Renderer>()->material->albedo = XMFLOAT4{ 71.f / 255.f, 102.f / 255.f, 0.f, 1 };
-	//}
+	GameObject* terrain = AddGameObject();
+	{
+		XMFLOAT3 scale(5.0f, 0.5f, 5.0f);
+		XMFLOAT4 color(0.0f, 0.2f, 0.0f, 0.0f);
+	
+		terrain->GetComponent<Transform>()->position.x = -64 * 5 * 2;
+		terrain->GetComponent<Transform>()->position.z = -64 * 5 * 2;
+		terrain->GetComponent<Transform>()->position.y = -100.f;
+		terrain->AddComponent<Terrain>()->LoadTerrain(L"heightMap.raw", 257, 257, 257, 257, scale, color);
+		terrain->AddComponent<Renderer>()->material = defaultMaterial;
+		terrain->GetComponent<Renderer>()->material->albedo = XMFLOAT4{ 71.f / 255.f, 102.f / 255.f, 0.f, 1 };
+	}
 
-	//GameObject* cube = AddGameObject();
-	//{
-	//	cube->GetComponent<Transform>()->position = Vector3(-50, 0, 0);
-	//	cube->AddComponent<MeshFilter>()->mesh = pCubeMesh;
-	//	cube->AddComponent<Renderer>()->material = defaultMaterial;
-	//	cube->AddComponent<RotatingBehavior>();
-	//}
+	GameObject* cube = CreateGameObject();
+	{
+		cube->AddComponent<MeshFilter>()->mesh = pCubeMesh;
+		cube->AddComponent<Renderer>()->material = defaultMaterial;
+		cube->AddComponent<RotatingBehavior>();
+		cube->AddComponent<MovingBehavior>();
+	}
 	
 	//GameObject* cube2 = CreateGameObject();
 	//cube->children.push_back(cube2);
@@ -38,12 +39,22 @@ void GameScene::BuildObjects()
 	//	cube2->AddComponent<RotatingBehavior>();
 	//}
 
-	GameObject* model = AddGameObject();
-	{
-		model->AddComponent<Renderer>()->material = defaultMaterial;
-		model->LoadGeometryFromFile("Model/Apache.bin");
-		model->AddComponent<RotatingBehavior>();
-	}
+	float interval = 150.f;
+	int xObjects = 1, yObjects = 0, zObjects = 1, i = 0;
+	for (int x = -xObjects; x <= xObjects; x++)
+		for (int y = -yObjects; y <= yObjects; y++)
+			for (int z = -zObjects; z <= zObjects; z++)
+			{
+				GameObject* model = AddGameObject();
+
+				model->transform->position = Vector3{ interval * x, interval * y, interval * z };
+
+				Material* material = model->AddComponent<Renderer>()->material = new DefaultMaterial();
+				material->albedo = RANDOM_COLOR;
+				material->shader = defaultMaterial->shader;
+				model->LoadGeometryFromFile("Model/Apache.bin");
+				model->AddComponent<RotatingBehavior>();
+			}
 	
 	//GameObject* _sceneManager = AddGameObject();
 	//{
