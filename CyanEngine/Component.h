5c0612da66608c22a7e5c3951611194305c517f0
HaneulYun <yhnil.kr@gmail.com>
2019-11-24 22:46:32 +0900

    sub 0.4.1 On~Tirgger/Collision~Enter/Stay/Exit 인터페이스 제공

--- CyanEngine/Component.h
+++ CyanEngine/Component.h
@@ -1,5 +1,8 @@
 #pragma once
 
+class Collision;
+class Collider;
+
 class Component : public Object
 {
 private:
@@ -19,6 +22,13 @@ public:
 
 	virtual void Start() {}
 	virtual void Update() {}
+
+	virtual void OnTriggerEnter(Collision* collision) {}
+	virtual void OnTriggerStay(Collision* collision) {}
+	virtual void OnTriggerExit(Collision* collision) {}
+	virtual void OnCollisionEnter(Collider* other) {}
+	virtual void OnCollisionStay(Collider* other) {}
+	virtual void OnCollisionExit(Collider* other) {}
 
 	virtual Component* Duplicate(Component* component) { return new Component(*component); }
 };
