de092a5fd7df4dfc9e766604f7aa44baf26c9fa3
HaneulYun <yhnil.kr@gmail.com>
2019-11-27 03:18:04 +0900

    sub 0.4.15 Camera 컴포넌트에서 ScreenToWorldPoint 제공

--- CyanEngine/Camera.cpp
+++ CyanEngine/Camera.cpp
@@ -5,10 +5,8 @@ Camera* Camera::main{ nullptr };
 
 Camera::Camera()
 {
-	m_xmf4x4View = NS_Matrix4x4::Identity();
-	m_xmf4x4Projection = NS_Matrix4x4::Identity();
-	m_d3dViewport = { 0, 0, FRAME_BUFFER_WIDTH , FRAME_BUFFER_HEIGHT, 0.0f, 1.0f };
-	m_d3dScissorRect = { 0, 0, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT };
+	view = NS_Matrix4x4::Identity();
+	projection = NS_Matrix4x4::Identity();
 }
 
 Camera::~Camera()
@@ -18,44 +16,57 @@ Camera::~Camera()
 void Camera::UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList)
 {
 	XMFLOAT4X4 xmf4x4View;
-	XMStoreFloat4x4(&xmf4x4View, XMMatrixTranspose(XMLoadFloat4x4(&m_xmf4x4View)));
+	XMStoreFloat4x4(&xmf4x4View, XMMatrixTranspose(XMLoadFloat4x4(&view)));
 	pd3dCommandList->SetGraphicsRoot32BitConstants(1, 16, &xmf4x4View, 0);
 
 	XMFLOAT4X4 xmf4x4Projection;
-	XMStoreFloat4x4(&xmf4x4Projection, XMMatrixTranspose(XMLoadFloat4x4(&m_xmf4x4Projection)));
+	XMStoreFloat4x4(&xmf4x4Projection, XMMatrixTranspose(XMLoadFloat4x4(&projection)));
 	pd3dCommandList->SetGraphicsRoot32BitConstants(1, 16, &xmf4x4Projection, 16);
 }
 
 void Camera::GenerateViewMatrix(XMFLOAT3 xmf3Position, XMFLOAT3 xmf3LookAt, XMFLOAT3 xmf3Up)
 {
-	m_xmf4x4View = NS_Matrix4x4::LookAtLH(xmf3Position, xmf3LookAt, xmf3Up);
+	view = NS_Matrix4x4::LookAtLH(xmf3Position, xmf3LookAt, xmf3Up);
 }
 
 void Camera::GenerateProjectionMatrix(float fNearPlaneDistance, float fFarPlaneDistance, float fAspectRatio, float fFOVAngle)
 {
-	m_xmf4x4Projection = NS_Matrix4x4::PerspectiveFovLH(XMConvertToRadians(fFOVAngle), fAspectRatio, fNearPlaneDistance, fFarPlaneDistance);
+	projection = NS_Matrix4x4::PerspectiveFovLH(XMConvertToRadians(fFOVAngle), fAspectRatio, fNearPlaneDistance, fFarPlaneDistance);
 }
 
 void Camera::SetViewport(int xTopLeft, int yTopLeft, int nWidth, int nHeight, float fMinZ, float fMaxZ)
 {
-	m_d3dViewport.TopLeftX = float(xTopLeft);
-	m_d3dViewport.TopLeftY = float(yTopLeft);
-	m_d3dViewport.Width = float(nWidth);
-	m_d3dViewport.Height = float(nHeight);
-	m_d3dViewport.MinDepth = fMinZ;
-	m_d3dViewport.MaxDepth = fMaxZ;
+	viewport.TopLeftX = float(xTopLeft);
+	viewport.TopLeftY = float(yTopLeft);
+	viewport.Width = float(nWidth);
+	viewport.Height = float(nHeight);
+	viewport.MinDepth = fMinZ;
+	viewport.MaxDepth = fMaxZ;
 }
 
 void Camera::SetScissorRect(LONG xLeft, LONG yTop, LONG xRight, LONG yBottom)
 {
-	m_d3dScissorRect.left = xLeft;
-	m_d3dScissorRect.top = yTop;
-	m_d3dScissorRect.right = xRight;
-	m_d3dScissorRect.bottom = yBottom;
+	scissorRect.left = xLeft;
+	scissorRect.top = yTop;
+	scissorRect.right = xRight;
+	scissorRect.bottom = yBottom;
 }
 
 void Camera::SetViewportsAndScissorRects(ID3D12GraphicsCommandList* pd3dCommandList)
 {
-	pd3dCommandList->RSSetViewports(1, &m_d3dViewport);
-	pd3dCommandList->RSSetScissorRects(1, &m_d3dScissorRect);
+	pd3dCommandList->RSSetViewports(1, &viewport);
+	pd3dCommandList->RSSetScissorRects(1, &scissorRect);
+}
+
+Vector3 Camera::ScreenToWorldPoint(Vector3 position)
+{
+	position.x = position.x / viewport.Width * 2 - 1;
+	position.y = position.y / viewport.Height * 2 - 1;
+	position.y = -position.y;
+
+	Vector3 vector;
+	XMStoreFloat3(&vector.xmf3, XMVector3Transform(XMLoadFloat3(&position.xmf3), XMMatrixInverse(NULL, XMLoadFloat4x4(&projection))));
+	XMStoreFloat3(&vector.xmf3, XMVector3Transform(XMLoadFloat3(&vector.xmf3), XMMatrixInverse(NULL, XMLoadFloat4x4(&view))));
+
+	return vector;
 }
