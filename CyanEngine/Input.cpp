a9d2738bc90e065411c283f20f3e0cdcb6d08692
HaneulYun <yhnil.kr@gmail.com>
2019-12-08 21:24:41 +0900

    sub 0.5.5 Input 관련 인터페이스 확장

--- CyanEngine/Input.cpp
+++ CyanEngine/Input.cpp
@@ -3,7 +3,10 @@
 
 Vector3 Input::mousePosition;
 bool Input::keys[256];
+bool Input::keyUp[256];
 bool Input::keyDown[256];
+bool Input::mouses[3];
+bool Input::mouseUp[3];
 bool Input::mouseDown[3];
 
 Input::Input()
@@ -27,9 +30,24 @@ bool Input::GetKey(KeyCode key)
 	return keys[(int)key];
 }
 
+bool Input::GetKeyUp(KeyCode key)
+{
+	return keyUp[(int)key];
+}
+
 bool Input::GetKeyDown(KeyCode key)
 {
 	return keyDown[(int)key];
+}
+
+bool Input::GetMouseButton(int button)
+{
+	return mouses[button];
+}
+
+bool Input::GetMouseButtonUp(int button)
+{
+	return mouseUp[button];
 }
 
 bool Input::GetMouseButtonDown(int button)
