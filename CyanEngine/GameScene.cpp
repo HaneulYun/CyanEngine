4a21ed139236d29a32c9e908f4ff8d3e1580cd31
jeehye <wangjh79@naver.com>
2019-12-09 01:12:06 +0900

    0.2.9 지혜 0.1) BigComingRect Speed 변경

--- CyanEngine/GameScene.cpp
+++ CyanEngine/GameScene.cpp
@@ -138,7 +138,7 @@ void GameScene::BuildObjects()
 		enemy2->GetComponent<MeshFilter>()->mesh = BigMesh;
 		enemy2->AddComponent<Enemy>();
 		enemy2->AddComponent<MovingBehavior>()->target = star->GetComponent<Transform>()->position;
-		enemy2->GetComponent<MovingBehavior>()->speed = 5.f;
+		enemy2->GetComponent<MovingBehavior>()->speed = 8.f;
 		enemy2->AddComponent<Damageable>()->SetHealth(32);
 		enemy2->GetComponent<Damageable>()->isTeam = false;
 		enemy2->AddComponent<BoxCollider>()->extents = Vector3{ 17.5f,17.5f,17.5f };
