c1ef1c55794b47f8368f854b3c87065c88258ec5
HaneulYun <yhnil.kr@gmail.com>
2019-12-08 22:31:38 +0900

    0.2.7 하늘0.3) MainThread 정리

--- MagentaServer/MainThread.cpp
+++ MagentaServer/MainThread.cpp
@@ -4,7 +4,7 @@
 extern MagentaFW gMagentaFW;
 
 MainThread::MainThread(int tId, LPVOID fParam)
-	: Thread(tId, Calculate, fParam)
+	: Thread(tId, MainThread::Calculate, fParam)
 {
 
 }
@@ -14,7 +14,7 @@ MainThread::~MainThread()
 
 }
 
-DWORD WINAPI Calculate(LPVOID arg)	// 임시 함수 이름
+DWORD WINAPI MainThread::Calculate(LPVOID arg)	// 임시 함수 이름
 {
 	Message result;
 
@@ -34,81 +34,50 @@ DWORD WINAPI Calculate(LPVOID arg)	// 임시 함수 이름
 			case MESSAGE_READY:
 				ThreadPool::clients[curMessage.lParam]->isReady = true;
 				if (ThreadPool::isAllClientsReady())
-				{
-					result.msgId = MESSAGE_GAME_START;
-					result.lParam = 0;
-					result.mParam = 0;
-					result.rParam = 0;
-					EnterCriticalSection(&ThreadPool::sqcs);
-					ThreadPool::sendQueue.push(result);
-					LeaveCriticalSection(&ThreadPool::sqcs);
-				}
+					RequestBulletCreation(result, curMessage, MESSAGE_GAME_START);
 				break;
 			case MESSAGE_REQUEST_BULLET_CREATION:
-				result.msgId = MESSAGE_CREATE_BULLET;
-				result.lParam = curMessage.lParam;//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(0);
-				result.mParam = curMessage.mParam;
-				result.rParam = curMessage.rParam;
-				EnterCriticalSection(&ThreadPool::sqcs);
-				ThreadPool::sendQueue.push(result);
-				LeaveCriticalSection(&ThreadPool::sqcs);
+				RequestBulletCreation(result, curMessage, MESSAGE_CREATE_BULLET);
+				//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(0);
 				break;
 			case MESSAGE_REQUEST_BULLET_CREATION_STRAIGHT:
-				result.msgId = MESSAGE_CREATE_BULLET_STRAIGHT;
-				result.lParam = curMessage.lParam;//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(0);
-				result.mParam = curMessage.mParam;
-				result.rParam = curMessage.rParam;
-				EnterCriticalSection(&ThreadPool::sqcs);
-				ThreadPool::sendQueue.push(result);
-				LeaveCriticalSection(&ThreadPool::sqcs);
+				RequestBulletCreation(result, curMessage, MESSAGE_CREATE_BULLET_STRAIGHT);
+				//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(0);
 				break;
 			case MESSAGE_REQUEST_BULLET_CREATION_CANNON:
-				result.msgId = MESSAGE_CREATE_BULLET_CANNON;
-				result.lParam = curMessage.lParam;//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(1);
-				result.mParam = curMessage.mParam;
-				result.rParam = curMessage.rParam;
-				EnterCriticalSection(&ThreadPool::sqcs);
-				ThreadPool::sendQueue.push(result);
-				LeaveCriticalSection(&ThreadPool::sqcs);
+				RequestBulletCreation(result, curMessage, MESSAGE_CREATE_BULLET_CANNON);
+				//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(1);
 				break;
 			case MESSAGE_REQUEST_BULLET_CREATION_SHARP:
-				result.msgId = MESSAGE_CREATE_BULLET_SHARP;
-				result.lParam = curMessage.lParam;//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(2);
-				result.mParam = curMessage.mParam;
-				result.rParam = curMessage.rParam;
-				EnterCriticalSection(&ThreadPool::sqcs);
-				ThreadPool::sendQueue.push(result);
-				LeaveCriticalSection(&ThreadPool::sqcs);
+				RequestBulletCreation(result, curMessage, MESSAGE_CREATE_BULLET_SHARP);
+				//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(2);
 				break;
 			case MESSAGE_REQUEST_BULLET_CREATION_LASER:
-				result.msgId = MESSAGE_CREATE_BULLET_LASER;
-				result.lParam = curMessage.lParam;//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(3);
-				result.mParam = curMessage.mParam;
-				result.rParam = curMessage.rParam;
-				EnterCriticalSection(&ThreadPool::sqcs);
-				ThreadPool::sendQueue.push(result);
-				LeaveCriticalSection(&ThreadPool::sqcs);
+				RequestBulletCreation(result, curMessage, MESSAGE_CREATE_BULLET_LASER);
+				//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(3);
 				break;
 			case MESSAGE_REQUEST_BULLET_CREATION_GUIDED:
-				result.msgId = MESSAGE_CREATE_BULLET_GUIDED;
-				result.lParam = curMessage.lParam;//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(4);
-				result.mParam = curMessage.mParam;
-				result.rParam = curMessage.rParam;
-				EnterCriticalSection(&ThreadPool::sqcs);
-				ThreadPool::sendQueue.push(result);
-				LeaveCriticalSection(&ThreadPool::sqcs);
+				RequestBulletCreation(result, curMessage, MESSAGE_CREATE_BULLET_GUIDED);
+				//ThreadPool::bulletGenerator->GetComponent<BulletGenerator>()->CreateBulletAndGetObjID(4);
 				break;
+
 			case MESSAGE_NOTIFY_COLLISION_BULLET_AND_ENEMY:
-				result.msgId = MESSAGE_NOTIFY_COLLISION_BULLET_AND_ENEMY;
-				result.lParam = curMessage.lParam;
-				result.mParam = curMessage.mParam;
-				result.rParam = curMessage.rParam;
-				EnterCriticalSection(&ThreadPool::sqcs);
-				ThreadPool::sendQueue.push(result);
-				LeaveCriticalSection(&ThreadPool::sqcs);
+				RequestBulletCreation(result, curMessage, MESSAGE_NOTIFY_COLLISION_BULLET_AND_ENEMY);
 				break;
 			}
 		}
 	}
 	return 0;
 }
+
+void MainThread::RequestBulletCreation(Message& result, Message& msg, unsigned char id)
+{
+	result.msgId = id;
+	result.lParam = msg.lParam;
+	result.mParam = msg.mParam;
+	result.rParam = msg.rParam;
+	EnterCriticalSection(&ThreadPool::sqcs);
+	ThreadPool::sendQueue.push(result);
+	LeaveCriticalSection(&ThreadPool::sqcs);
+}
+
