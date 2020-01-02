639cb0b734a6037cd4ad7da15d2c4b2037697267
HaneulYun <yhnil.kr@gmail.com>
2020-01-02 11:28:57 +0900

    sub 0.6.5 FbxManager 초기화

--- CyanEngine/FBX_TEST.h
+++ CyanEngine/FBX_TEST.h
@@ -6,7 +6,8 @@ class FBX_TEST : public MonoBehavior<FBX_TEST>
 private /*이 영역에 private 변수를 선언하세요.*/:
 
 public  /*이 영역에 public 변수를 선언하세요.*/:
-	
+	FbxManager* fbxManager{ nullptr };
+	FbxScene* fbxScene{ nullptr };
 
 private:
 	friend class GameObject;
@@ -19,6 +20,17 @@ public:
 
 	void Start(/*초기화 코드를 작성하세요.*/)
 	{
+		fbxManager = FbxManager::Create();
+		if (!fbxManager)
+		{
+			Debug::Log("Error: Unable to create FBX Manager!\n");
+		}
+		else
+		{
+			char str[100];
+			sprintf(str, "Autodesk FBX SDK version %s\n", fbxManager->GetVersion());
+			Debug::Log(str);
+		}
 	}
 
 	void Update(/*업데이트 코드를 작성하세요.*/)
