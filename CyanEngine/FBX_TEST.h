e8ea9d39e4c7034c571912cf6cbafc70f4d03656
jeehye <wangjh79@naver.com>
2020-01-07 20:02:32 +0900

    0.1 FBX Mesh의 정점, 인덱스 정보 불러오기

--- CyanEngine/FBX_TEST.h
+++ CyanEngine/FBX_TEST.h
@@ -9,6 +9,12 @@ public  /*이 영역에 public 변수를 선언하세요.*/:
 	FbxManager* fbxManager{ nullptr };
 	FbxScene* fbxScene{ nullptr };
 	FbxImporter* fbxImporter{ nullptr };
+	FbxMesh* fbxMesh{ nullptr };
+
+	XMFLOAT3* controlPoints = NULL;
+	XMFLOAT4* colors = NULL;
+	XMFLOAT3* normals = NULL;
+	UINT* indices = NULL;
 
 private:
 	friend class GameObject;
@@ -52,7 +58,7 @@ public:
 			{
 				message = "Importing file ";
 				message += url;
-				message += "\nPlease wait!";
+				message += "\nPlease wait!\n";
 
 				// Set scene status flag to ready to load.
 				//mStatus = MUST_BE_LOADED;
@@ -75,10 +81,79 @@ public:
 		Debug::Log(message.c_str());
 
 		fbxImporter->Import(fbxScene);
+		
+		fbxImporter->Destroy();
+
+
+		FbxGeometryConverter geometryConverter(fbxManager);
+		geometryConverter.Triangulate(fbxScene, true);
+
+		LoadMesh();
 	}
 
 	void Update(/*업데이트 코드를 작성하세요.*/)
 	{
+
+	}
+
+	void FindMesh(FbxNode* node)
+	{
+		FbxNodeAttribute* nodeAttribute = node->GetNodeAttribute();
+
+		if (nodeAttribute) {
+			if (nodeAttribute->GetAttributeType() == FbxNodeAttribute::eMesh)
+			{
+				fbxMesh = node->GetMesh();
+				Debug::Log("Finding Mesh Success\n");
+				return;
+			}
+		}
+		const int childCount = node->GetChildCount();
+
+		for (unsigned int i = 0; i < childCount; ++i)
+			FindMesh(node->GetChild(i));
+	};
+
+	void ProcessControlPoints(FbxMesh* mesh)
+	{
+		int count = mesh->GetControlPointsCount();
+		controlPoints = new XMFLOAT3[count];
+
+		for (int i = 0; i < count; ++i)
+		{
+			XMFLOAT3 pos;
+			pos.x = static_cast<float>(mesh->GetControlPointAt(i).mData[0]);
+			pos.y = static_cast<float>(mesh->GetControlPointAt(i).mData[1]);
+			pos.z = static_cast<float>(mesh->GetControlPointAt(i).mData[2]);
+
+			controlPoints[i] = pos;
+		}
+	}
+
+	void LoadMesh()
+	{
+		FbxNode* rootNode = fbxScene->GetRootNode();
+		FindMesh(rootNode);
+
+		// 제어점 위치 찾기
+		ProcessControlPoints(fbxMesh);
+
+		// 제어점 인덱스 찾기
+		int triangleCount = fbxMesh->GetPolygonCount();
+		int vertexCount = 0;
+
+		indices = new UINT[triangleCount * 3];
+
+
+		for (int i = 0; i < triangleCount; ++i) 
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				int controlPointIndex = fbxMesh->GetPolygonVertex(i, j);
+				indices[vertexCount++] = controlPointIndex;
+			}
+		}
+	
 	}
 
 	// 필요한 경우 함수를 선언 및 정의 하셔도 됩니다.
