00ae32e0e8cb2a380a34f000c52c22295271926a
HaneulYun <yhnil.kr@gmail.com>
2020-02-14 13:10:24 +0900

    sub 0.7.43 확장성을 위해 쉐이더의 b1 의 passConstants 에서 더 많은 정보를 제공할 수 있도록 쉐이더 수정

--- CyanEngine/Shaders/shaders.hlsl
+++ CyanEngine/Shaders/shaders.hlsl
@@ -1,11 +1,28 @@
-cbuffer SceneConstantBuffer : register(b0)
-{
-	float4x4 world;
-}
-
 cbuffer cbPass : register(b1)
 {
 	float4x4 mtx;
+
+	float4x4 gView;
+	float4x4 gInvView;
+	float4x4 gProj;
+	float4x4 gInvProj;
+	float4x4 gViewProj;
+	float4x4 gInvViewProj;
+
+	float3 gEyePosW;
+	float  cbPerObjectPad1;
+	float2 gRenderTargetSize;
+	float2 gInvRenderTargetSize;
+
+	float gNearZ;
+	float gFarZ;
+	float gTotalTime;
+	float gDeltaTime;
+}
+
+cbuffer cbPerObject : register(b0)
+{
+	float4x4 gWorld;
 }
 
 struct PSInput
