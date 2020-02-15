199c7d7e696bfcd0bd1f1eb1e8cacb4d920824cb
HaneulYun <yhnil.kr@gmail.com>
2020-02-15 23:37:17 +0900

    sub 0.8.a.1 재질 적용

--- CyanEngine/RendererManager.cpp
+++ CyanEngine/RendererManager.cpp
@@ -514,28 +514,48 @@ void RendererManager::LoadAssets()
 		geometries[geo->Name] = std::move(geo);
 	}
 	{
-		auto grass = std::make_unique<Material>();
-		grass->Name = "grass";
-		grass->MatCBIndex = 0;
-		grass->DiffuseAlbedo = XMFLOAT4(0.2f, 0.6f, 0.2f, 1.0f);
-		grass->FresnelR0 = XMFLOAT3(0.01f, 0.01f, 0.01f);
-		grass->Roughness = 0.125f;
+		auto bricks0 = std::make_unique<Material>();
+		bricks0->Name = "bricks0";
+		bricks0->MatCBIndex = 0;
+		bricks0->DiffuseSrvHeapIndex = 0;
+		bricks0->DiffuseAlbedo = XMFLOAT4(Colors::ForestGreen);
+		bricks0->FresnelR0 = XMFLOAT3(0.02f, 0.02f, 0.02f);
+		bricks0->Roughness = 0.1f;
 
-		auto water = std::make_unique<Material>();
-		water->Name = "water";
-		water->MatCBIndex = 1;
-		water->DiffuseAlbedo = XMFLOAT4(0.0f, 0.2f, 0.6f, 1.0f);
-		water->FresnelR0 = XMFLOAT3(0.1f, 0.1f, 0.1f);
-		water->Roughness = 0.f;
+		auto stone0 = std::make_unique<Material>();
+		stone0->Name = "stone0";
+		stone0->MatCBIndex = 1;
+		stone0->DiffuseSrvHeapIndex = 1;
+		stone0->DiffuseAlbedo = XMFLOAT4(Colors::LightSteelBlue);
+		stone0->FresnelR0 = XMFLOAT3(0.05f, 0.05f, 0.05f);
+		stone0->Roughness = 0.3f;
 
-		materials["grass"] = std::move(grass);
-		materials["water"] = std::move(water);
+		auto tile0 = std::make_unique<Material>();
+		tile0->Name = "tile0";
+		tile0->MatCBIndex =2;
+		tile0->DiffuseSrvHeapIndex = 2;
+		tile0->DiffuseAlbedo = XMFLOAT4(Colors::LightGray	);
+		tile0->FresnelR0 = XMFLOAT3(0.02f, 0.02f, 0.02f);
+		tile0->Roughness = 0.2f;
+
+		auto skullMat = std::make_unique<Material>();
+		skullMat->Name = "skullMat";
+		skullMat->MatCBIndex = 3;
+		skullMat->DiffuseSrvHeapIndex = 3;
+		skullMat->DiffuseAlbedo = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		skullMat->FresnelR0 = XMFLOAT3(0.05f, 0.05f, 0.05);
+		skullMat->Roughness = 0.3f;
+
+		materials["bricks0"] = std::move(bricks0);
+		materials["stone0"] = std::move(stone0);
+		materials["tile0"] = std::move(tile0);
+		materials["skullMat"] = std::move(skullMat);
 	}
 	{
 		auto boxRItem = std::make_unique<RenderItem>();
 		XMStoreFloat4x4(&boxRItem->world, XMMatrixScaling(2.0f, 2.0f, 2.0f)* XMMatrixTranslation(0.0f, 0.5f, 0.0f));
 		boxRItem->objCBIndex = 0;
-		boxRItem->mat = materials["grass"].get();
+		boxRItem->mat = materials["stone0"].get();
 		boxRItem->geo = geometries["shapeGeo"].get();
 		boxRItem->primitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 		boxRItem->indexCount = boxRItem->geo->DrawArgs["box"].IndexCount;
@@ -546,7 +566,7 @@ void RendererManager::LoadAssets()
 		auto gridRItem = std::make_unique<RenderItem>();
 		gridRItem->world = MathHelper::Identity4x4();
 		gridRItem->objCBIndex = 1;
-		gridRItem->mat = materials["grass"].get();
+		gridRItem->mat = materials["tile0"].get();
 		gridRItem->geo = geometries["shapeGeo"].get();
 		gridRItem->primitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 		gridRItem->indexCount = gridRItem->geo->DrawArgs["grid"].IndexCount;
@@ -569,7 +589,7 @@ void RendererManager::LoadAssets()
 
 			XMStoreFloat4x4(&leftCylRItem->world, leftCylWorld);
 			leftCylRItem->objCBIndex = objCBIndex++;
-			leftCylRItem->mat = materials["grass"].get();
+			leftCylRItem->mat = materials["bricks0"].get();
 			leftCylRItem->geo = geometries["shapeGeo"].get();
 			leftCylRItem->primitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 			leftCylRItem->indexCount = leftCylRItem->geo->DrawArgs["cylinder"].IndexCount;
@@ -578,7 +598,7 @@ void RendererManager::LoadAssets()
 
 			XMStoreFloat4x4(&rightCylRItem->world, rightCylWorld);
 			rightCylRItem->objCBIndex = objCBIndex++;
-			rightCylRItem->mat = materials["grass"].get();
+			rightCylRItem->mat = materials["bricks0"].get();
 			rightCylRItem->geo = geometries["shapeGeo"].get();
 			rightCylRItem->primitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 			rightCylRItem->indexCount = rightCylRItem->geo->DrawArgs["cylinder"].IndexCount;
@@ -587,7 +607,7 @@ void RendererManager::LoadAssets()
 
 			XMStoreFloat4x4(&leftSphereRItem->world, leftSphereWorld);
 			leftSphereRItem->objCBIndex = objCBIndex++;
-			leftSphereRItem->mat = materials["grass"].get();
+			leftSphereRItem->mat = materials["stone0"].get();
 			leftSphereRItem->geo = geometries["shapeGeo"].get();
 			leftSphereRItem->primitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 			leftSphereRItem->indexCount = leftSphereRItem->geo->DrawArgs["sphere"].IndexCount;
@@ -596,7 +616,7 @@ void RendererManager::LoadAssets()
 
 			XMStoreFloat4x4(&rightSphereRItem->world, rightSphereWorld);
 			rightSphereRItem->objCBIndex = objCBIndex++;
-			rightSphereRItem->mat = materials["grass"].get();
+			rightSphereRItem->mat = materials["stone0"].get();
 			rightSphereRItem->geo = geometries["shapeGeo"].get();
 			rightSphereRItem->primitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 			rightSphereRItem->indexCount = rightSphereRItem->geo->DrawArgs["sphere"].IndexCount;
