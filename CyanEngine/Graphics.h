936cf3baa42bbcb44aedf44a6e317c372dce5e8c
HaneulYun <yhnil.kr@gmail.com>
2020-07-15 20:33:17 +0900

    sub 0.12.44 DS.6 패스버퍼를 이용한 태양 구현 (z축에 대해 반시계방향으로 회전하는 Directional 조명)

--- CyanEngine/Graphics.h
+++ CyanEngine/Graphics.h
@@ -61,13 +61,13 @@ public:
 	//std::unique_ptr<ShadowMap> shadowMap;
 	//DirectX::BoundingSphere sceneBounds;
 	//
-	//float lightRotationAngle = 0.0f;
-	//Vector3 baseLightDirections[3] = {
-	//	Vector3(0.57735f, -0.57735f, 0.57735f),
-	//	Vector3(-0.57735f, -0.57735f, 0.57735f),
-	//	Vector3(0.0f, -0.707f, -0.707f)
-	//};
-	//Vector3 rotatedLightDirections[3];
+	float lightRotationAngle = 0.0f;
+	Vector3 baseLightDirections[3] = {
+		Vector3(0, -1, 0),
+		Vector3(-0.57735f, -0.57735f, 0.57735f),
+		Vector3(0.0f, -0.707f, -0.707f)
+	};
+	Vector3 rotatedLightDirections[3];
 
 public:
 	Graphics() {}
