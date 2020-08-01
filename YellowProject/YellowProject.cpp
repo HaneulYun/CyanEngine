3deda89517f68e938aecd6e35222895bfed1416e
HaneulYun <yhnil.kr@gmail.com>
2020-08-02 08:32:31 +0900

    sub 0.13.27 MLD.0 74393 할당

--- YellowProject/YellowProject.cpp
+++ YellowProject/YellowProject.cpp
@@ -4,7 +4,7 @@
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int nCmdShow)
 {
 	CyanFW* cyanFW = new CyanFW(1280, 720, L"YellowProject");
-
+	
 	SceneManager* sceneManager = SceneManager::Instance();
 	sceneManager->scenes["MenuScene"] = new MenuScene();
 	sceneManager->scenes["TerrainScene"] = new TerrainScene();
@@ -12,5 +12,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int nCmdShow)
 	sceneManager->scenes["CharacterScene"] = new CharacterScene();
 	Scene::scene = sceneManager->scene = sceneManager->scenes["MenuScene"];
 
-	return CyanApp::Run(cyanFW, hInstance, nCmdShow);
+	CyanApp::Run(cyanFW, hInstance, nCmdShow);
+
+	delete cyanFW;
+
+	return 0;
 }
